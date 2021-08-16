72f8eb5265a6a258685281b51ef967d9736f6916
Zachary Sawyer <mountainsounds15@gmail.com>
2021-08-16 15:45:25 -0400

    cleaning up code

--- pages/about.js
+++ pages/about.js
@@ -13,10 +13,10 @@ const About = () => {
         <div className="about-me__content">
             <h2 className="about-me__content--title">About Me</h2>
             <p className="about-me__content--para">
-               Growing up in rural Vermont, I enjoyed New England's simple pleasures and more maple syrup than is probably good for one. While attaining a degree in Philosophy, far from the hills and granite cliffs of home, I became a climber. Upon graduating, I pursued training and certification with the American Mountain Guides Association and began to teach, coach, and guide new and experienced climbers alike throughout the U.S.
+               Growing up in rural Vermont, I enjoyed New England&apos;s simple pleasures and more maple syrup than is probably good for one. While attaining a degree in Philosophy, far from the hills and granite cliffs of home, I became a climber. Upon graduating, I pursued training and certification with the American Mountain Guides Association and began to teach, coach, and guide new and experienced climbers alike throughout the U.S.
             </p>
             <p className="about-me__content--para">
-                Teaching others to climb, I realized that learning any new skill simply required time, diligent practice, and effective teaching. I began to think about fresh, professional possibilities, and fell upon software development. Solving a bug, or attempting to implement an idea reminded me of the process of succeeding on a hard climb. It required a similar mix of methodical approach, personal style, and often, simply a good night's sleep!
+                Teaching others to climb, I realized that learning any new skill simply required time, diligent practice, and effective teaching. I began to think about fresh, professional possibilities, and fell upon software development. Solving a bug, or attempting to implement an idea reminded me of the process of succeeding on a hard climb. It required a similar mix of methodical approach, personal style, and often, simply a good night&apos;s sleep!
 
             </p>
             <p className="about-me__content--para">
@@ -24,9 +24,9 @@ const About = () => {
             </p>
 
             <p className="about-me__content--para">
-                I hope you enjoy your time here, and please don't hesitate to get in touch to chat, collaborate, or even spend a few hours climbing happily over warm rock!
+                I hope you enjoy your time here, and please don&apos;t hesitate to get in touch to chat, collaborate, or even spend a few hours climbing happily over warm rock!
             </p>
-                {/* <a target="_blank" href="https://www.saltpumpclimbing.com/author/zack-sawyer/" className="about-me__content--link">My Writings</a> */}
+
         </div>
       </section>
     </BaseLayout>
