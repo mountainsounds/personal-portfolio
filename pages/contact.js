6106e74d45c826467358fe3e45236c1e7a84ae3c
Zachary Sawyer <mountainsounds15@gmail.com>
2021-09-07 18:04:09 -0400

    refactored EmailJs to use react

--- pages/contact.js
+++ pages/contact.js
@@ -1,18 +1,52 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
+import emailjs from 'emailjs-com';
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  function notify(isSuccess) {
+    let successMsg = "Your message has been successfully sent. I look forward to connnecting soon!";
+    let errorMsg = "Apologies, your message was unsuccessfully sent. Try connecting by email: mountainsounds15@gmail.com";
+
+    isSuccess ? toast.success(successMsg) : toast.error(errorMsg);
+  }
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+       emailjs.sendForm(process.env.NEXT_PUBLIC_SERVICE_ID, process.env.NEXT_PUBLIC_TEMPLATE_ID, e.target, process.env.NEXT_PUBLIC_USER_ID)
+      .then((result) => {
+         notify(true);
+          setName("");
+          setEmail("");
+          setSubject("");
+          setMessage("");
+
+      }, (error) => {
+          notify(false);
+      });
+  }
+
   return (
       <BaseLayout page='contact'>
       <section id="contact">
-        <form action="" id="contact-form" className="contact__form">
-            <input type="text" placeholder="Name" name="name" id="name" required />
-            <input type="email" placeholder="Email" name="email" id="email" required />
-            <input type="text" placeholder="Subject" name="subject" id="subject" />
-            <textarea name="message" id="message" placeholder="Message"></textarea>
+        <form onSubmit={sendEmail} action="" id="contact-form" className="contact__form">
+            <input type="text" placeholder="Name" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+            <input type="email" placeholder="Email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <input type="text" placeholder="Subject" name="subject" id="subject" value={subject} onChange={(e) => setSubject(e.target.value)} />
+            <textarea name="message" id="message" placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
             <input type="submit" name="" id="" value="Submit" />
           </form>
 
         </section>
+        <ToastContainer />
       </BaseLayout>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import HomePage from '@/components/HomePage';
-import { useRouter } from 'next/router';
-
-
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <>
       <BaseLayout page='home'>
